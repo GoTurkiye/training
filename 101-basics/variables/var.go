2e70ae3a30ce396cee4514335e2af7e0212b55bd
huseyin.guner <huseyin.guner@trendyol.com>
2021-09-19 01:37:58 +0300

    101 Go Basics: Egitim icerikleri eklendi

---
+++ 101-basics/variables/var.go
@@ -0,0 +1,11 @@
+package main
+
+var degisken_1 string
+var degisken_2 = "deger 2"
+
+func main() {
+	degisken_1 = "deger 1"
+	degisken_3 := "deger 3"
+
+	println(degisken_1,degisken_2,degisken_3)
+}
