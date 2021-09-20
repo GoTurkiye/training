a888c84cedd9f82504fd38c7d6f296e2265abb0a
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-09-21 01:21:27 +0300

    [fixed-race-condition-pointers]

--- 102-concurrency/goroutines/race-condition-pointers.go
+++ 102-concurrency/goroutines/race-condition-pointers.go
@@ -9,14 +9,18 @@ type RaceTest struct {
 	Val int
 }
 
+var lock sync.Mutex
+
 func main() {
 	raceTest := &RaceTest{}
 
 	wg := &sync.WaitGroup{}
 	wg.Add(10000)
 
-	for i:=0; i<10000; i++ {
-		go increment(raceTest, wg)
+	for i := 0; i < 10000; i++ {
+		lock.Lock()
+		go incrementFixed(raceTest, wg)
+		//go increment(raceTest, wg)
 	}
 
 	wg.Wait()
@@ -26,5 +30,11 @@ func main() {
 
 func increment(rt *RaceTest, wg *sync.WaitGroup) {
 	rt.Val += 1
+	wg.Done()
+}
+
+func incrementFixed(rt *RaceTest, wg *sync.WaitGroup) {
+	rt.Val += 1
+	lock.Unlock()
 	wg.Done()
 }
