425aa7cb975c479297e64078674a4f4d1e4ee18a
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2021-09-23 20:27:22 +0300

    feat: implement new type for string slice
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

--- 202-cli/flag101/subcommand.go
+++ 202-cli/flag101/subcommand.go
@@ -4,12 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// Create a new type for a list of Strings
+type stringList []string
+
+// Implement the flag.Value interface
+func (s *stringList) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+func (s *stringList) Set(value string) error {
+	*s = strings.Split(value, ",")
+	return nil
+}
 
 func main() {
 	subOne := flag.NewFlagSet("one", flag.ExitOnError)
 	oneCream := subOne.Bool("cream", false, "Cream")
-	oneSugar := subOne.String("sugar", "", "Sugar")
+	var sugar stringList
+	subOne.Var(&sugar, "sugar", "")
 
 	subTwo := flag.NewFlagSet("two", flag.ExitOnError)
 	twoTea := subTwo.Int("tea", 0, "Tea")
@@ -25,7 +40,7 @@ func main() {
 		subOne.Parse(os.Args[2:])
 		fmt.Println("subcommand 'one'")
 		fmt.Println("  Cream:", *oneCream)
-		fmt.Println("  Sugar:", *oneSugar)
+		fmt.Println("  Sugar:", sugar.String())
 		fmt.Println("  tail:", subOne.Args())
 	case "two":
 		subTwo.Parse(os.Args[2:])
